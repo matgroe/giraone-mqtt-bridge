c1887535e9f378204ac13e536a98eb9315acd03e
matthias <matgroe@gmail.com>
2026-04-23 15:19:03 +0000

    set some env in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
+MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_USER=$(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::services mqtt "password")
+
 java -jar app.jar
-
-
-echo "Hello world!"
