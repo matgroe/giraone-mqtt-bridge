4eef45f48faf54c00cde3db16ce189dd69c8ab48
matthias <matgroe@gmail.com>
2026-04-24 08:55:52 +0000

    Renamed some configuration variables

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bashio
+echo
+echo '###########################################################################################################'
+echo '   Starting Container '
 
-MQTT_BROKER=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
+export
+
+if ! bashio::services.available "mqtt" && ! bashio::config.exists 'mqtt_server'; then
+    bashio::exit.nok "No internal MQTT service found and no MQTT server defined. Please install Mosquitto broker or specify your own."
+else
+    bashio::log.info "MQTT available, fetching server detail ..."
+fi
+#export MQTT_BROKER=$(bashio::services mqtt "host")
+#export MQTT_PORT=$(bashio::services mqtt "port")
+#export MQTT_USER=$(bashio::services mqtt "username")
+#export MQTT_PASSWORD=$(bashio::services mqtt "password")
+
+#export XMQTT_BROKER=$(bashio::services mqtt "host")
+#export XMQTT_USER=$(bashio::services mqtt "username")
+#export XMQTT_PASSWORD=$(bashio::services mqtt "password")
+
 
 java -jar app.jar
