9d16ea246de2f22818a31c890f5406dfd5bade22
matthias <matgroe@gmail.com>
2026-04-24 12:16:39 +0000

    Extended DockerEntrypoint.sh

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -9,18 +9,23 @@ export GIRAONE_USER=$(bashio::config 'GIRA_USER')
 export GIRAONE_PASSWORD=$(bashio::config 'GIRA_PASSWORD')
 export LOGGING_LEVEL_DE_MATGROE=$(bashio::config 'LOG_LEVEL')
 
-if ! bashio::services.available "mqtt"; then
+export MQTT_USE_INTERNAL_BROKER=$(bashio::config 'MQTT_USE_INTERNAL_BROKER')
+export MQTT_BROKER=$(bashio::config 'MQTT_BROKER')
+export MQTT_PORT=$(bashio::config 'MQTT_PORT')
+export MQTT_USER=$(bashio::config 'MQTT_USER')
+export MQTT_PASSWORD=$(bashio::config 'MQTT_PASSWORD')
+
+if [ -n "${MQTT_USE_INTERNAL_BROKER}" ]; then
+  if bashio::services.available "mqtt"; then
    bashio::exit.nok "No internal MQTT Broker found. Please install Mosquitto broker."
-else
+  else
     export MQTT_BROKER=$(bashio::services mqtt "host")
     export MQTT_PORT=$(bashio::services mqtt "port")
     export MQTT_USER=$(bashio::services mqtt "username")
     export MQTT_PASSWORD=$(bashio::services mqtt "password")
     bashio::log.info "Configured'$MQTT_BROKER' mqtt broker."
+  fi
 fi
-
-echo '- Environment Settings:'
-export
 
 echo '- Starting Application:'
 java -jar app.jar
