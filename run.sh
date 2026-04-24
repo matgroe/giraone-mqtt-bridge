69d49e06c8c9c92473dac4ce933bc44b88be2934
matthias <matgroe@gmail.com>
2026-04-24 06:34:03 +0000

    Renamed some configuration variables

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_BROKER=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 
