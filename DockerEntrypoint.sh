ffafa945a276e6ae9b134dc628d6b2bbc54a4b5c
matthias <matgroe@gmail.com>
2026-04-24 09:33:28 +0000

    Renamed some configuration variables

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -4,10 +4,13 @@ echo
 echo '###########################################################################################################'
 echo '- Preparing Environment Settings'
 
-export MQTT_HOST=$(bashio::config 'Mqtt_host')
-export MQTT_PORT=$(bashio::config 'Mqtt_port')
-export MQTT_USER=$(bashio::config 'Mqtt_user')
-export MQTT_PASS=$(bashio::config 'Mqtt_pass')
+set
+
+export GIRA_SERVER=$(bashio::config 'GIRA_SERVER')
+export GIRA_USER=$(bashio::config 'GIRA_USER')
+export GIRA_PASSWORD=$(bashio::config 'GIRA_PASSWORD')
+export LOG_LEVEL=$(bashio::config 'LOG_LEVEL')
+
 
 if ! bashio::services.available "mqtt"; then
    bashio::exit.nok "No internal MQTT Broker found. Please install Mosquitto broker."
